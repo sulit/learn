4457129db463e2bbf417764eaedc4ef22250bce1
sulit <sulitsrc@163.com>
2014-10-29 07:09:03 +0800

    change mygetopt.c

--- mystrrch.c
+++ mystrrch.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	char string[20];
-	char *ptr, c = 'r';
-	strcpy(string, "There are two rings");
+	char *ptr, c = ':';
+	strcpy(string, "[192.168.11.11:]80");
 	ptr = strrchr(string, c);
 	if (ptr)
 	  printf("The character %c is at position: %s\n", c, ptr);
