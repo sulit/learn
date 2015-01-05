cbde903dd5e89566448ce9095872ecf93026baf2
sulit <sulitsrc@163.com>
2015-01-04 23:15:04 -0500

    add k&r c program

---
+++ 1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world\b\n");
+	printf("\c");
+
+	return 0;
+}
---
+++ myexpress.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int n = 3;
+	float f = 4.0;
+	printf("%d\n", (n < 0) ? f : n);
+
+	return 0;
+}
