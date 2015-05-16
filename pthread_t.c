45ed823108d181d59958a505645456332ecceadd
sulit <sulitsrc@163.com>
2015-05-17 01:01:29 +0800

    thread exit style program

---
+++ pthread_t.c
@@ -0,0 +1,56 @@
+/*============================================
+ *   Copyright (C) 2015 All rights reserved.
+ *
+ *   filename : pthread_t.c
+ *   author : sulit - sulitsrc@163.com
+ *   last modified : 2015-05-17 00:59
+ *   description :
+ *
+ **============================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *thr_fn1(void *arg)
+{
+	printf("thread 1 returning\n");
+	return (void *)1;
+}
+
+void *thr_fn2(void *arg)
+{
+	printf("thread 2 exiting\n");
+	pthread_exit((void *)2);
+}
+
+void *thr_fn3(void *arg)
+{
+	while(1) {
+		printf("thread 3 writing\n");
+		sleep(1);
+	}
+}
+
+int main(void)
+{
+	pthread_t   tid;
+	void        *tret;
+
+	pthread_create(&tid, NULL, thr_fn1, NULL);
+	pthread_join(tid, &tret);
+	printf("thread 1 exit code %d\n", (int)tret);
+
+	pthread_create(&tid, NULL, thr_fn2, NULL);
+	pthread_join(tid, &tret);
+	printf("thread 2 exit code %d\n", (int)tret);
+
+	pthread_create(&tid, NULL, thr_fn3, NULL);
+	sleep(3);
+	pthread_cancel(tid);
+	pthread_join(tid, &tret);
+	printf("thread 3 exit code %d\n", (int)tret);
+
+	return 0;
+}
