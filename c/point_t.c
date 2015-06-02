17382f8ab4054b0ee6f22d4d5b9c08268b5c4ca5
sulit <sulitsrc@163.com>
2015-06-02 23:30:50 +0800

    add a interest program

---
+++ c/point_t.c
@@ -0,0 +1,20 @@
+/*============================================
+ *   Copyright (C) 2015 All rights reserved.
+ *
+ *   filename : point_t.c
+ *   author : sulit - sulitsrc@163.com
+ *   last modified : 2015-06-02 23:28
+ *   description : some meaning for pointer
+ *
+ *============================================*/
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	printf("**********" + 10);
+	printf("\n");
+	printf("**********" + 0);
+	printf("\n");
+}
